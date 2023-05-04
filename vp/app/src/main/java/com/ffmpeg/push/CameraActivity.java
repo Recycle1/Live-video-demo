4c8f83186a6e19324116da71bec595197b32e9cf
Recycle1 <501912318@qq.com>
2023-05-04 11:16:56 +0800

    2023-05-04 code

---
+++ vp/app/src/main/java/com/ffmpeg/push/CameraActivity.java
@@ -0,0 +1,342 @@
+package com.ffmpeg.push;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.github.faucamp.simplertmp.RtmpHandler;
+import com.seu.magicfilter.utils.MagicFilterType;
+
+import net.ossrs.yasea.SrsCameraView;
+import net.ossrs.yasea.SrsEncodeHandler;
+import net.ossrs.yasea.SrsPublisher;
+import net.ossrs.yasea.SrsRecordHandler;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+public class CameraActivity extends Activity implements RtmpHandler.RtmpListener, SrsRecordHandler.SrsRecordListener, SrsEncodeHandler.SrsEncodeListener,View.OnClickListener{
+    private static final String TAG = "CameraActivity";
+
+    public final static int RC_CAMERA = 100;
+    private boolean isPermissionGranted = false;
+    private Button mPublishBtn;
+    private Button mCameraSwitchBtn;
+    private Button mEncoderBtn;
+    private Button btnRecord;
+    private EditText mRempUrlEt;
+    private SrsPublisher mPublisher;
+    private String rtmpUrl;
+    private String path;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_camera);
+
+        requestPermission();
+
+        mPublishBtn = (Button) findViewById(R.id.publish);
+        mCameraSwitchBtn = (Button) findViewById(R.id.swCam);
+        mEncoderBtn = (Button) findViewById(R.id.swEnc);
+        mRempUrlEt = (EditText) findViewById(R.id.url);
+        mPublishBtn.setOnClickListener(this);
+        mCameraSwitchBtn.setOnClickListener(this);
+        mEncoderBtn.setOnClickListener(this);
+
+        //MainActivity.stream(path,"rtmp://101.201.109.91:1935/live/livestream");
+        mPublisher = new SrsPublisher((SrsCameraView) findViewById(R.id.glsurfaceview_camera));
+        //mPublisher=new SrsPublisher();
+        //编码状态回调
+        mPublisher.setEncodeHandler(new SrsEncodeHandler(this));
+        mPublisher.setRecordHandler(new SrsRecordHandler(this));
+        //rtmp推流状态回调
+        mPublisher.setRtmpHandler(new RtmpHandler(this));
+        //预览分辨率
+        mPublisher.setPreviewResolution(1280, 720);
+        //推流分辨率
+        mPublisher.setOutputResolution(720, 1280);
+        //传输率
+        mPublisher.setVideoHDMode();
+        //开启美颜（其他滤镜效果在MagicFilterType中查看）
+        mPublisher.switchCameraFilter(MagicFilterType.BEAUTY);
+        //打开摄像头，开始预览（未推流）
+        mPublisher.startCamera();
+        //btnRecord = (Button) findViewById(R.id.record);
+        path= Environment.getExternalStorageDirectory().getAbsolutePath()+ "/test1.mp4";
+        btnRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnRecord.getText().toString().contentEquals("record")) {
+                    if (mPublisher.startRecord(path)) {
+                        btnRecord.setText("pause");
+                    }
+                } else if (btnRecord.getText().toString().contentEquals("pause")) {
+                    mPublisher.pauseRecord();
+                    btnRecord.setText("resume");
+                } else if (btnRecord.getText().toString().contentEquals("resume")) {
+                    mPublisher.resumeRecord();
+                    btnRecord.setText("pause");
+                }
+            }
+        });
+
+    }
+
+//    public native int stream(String inputurl, String outputurl);
+//
+//    static {
+//        System.loadLibrary("avutil-54");
+//        System.loadLibrary("swresample-1");
+//        System.loadLibrary("avcodec-56");
+//        System.loadLibrary("avformat-56");
+//        System.loadLibrary("swscale-3");
+//        System.loadLibrary("postproc-53");
+//        System.loadLibrary("avfilter-5");
+//        System.loadLibrary("avdevice-56");
+//        System.loadLibrary("sffstreamer");
+//    }
+
+    private void requestPermission() {
+        //1. 检查是否已经有该权限
+        if (Build.VERSION.SDK_INT >= 23 && (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)) {
+            //2. 权限没有开启，请求权限
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, RC_CAMERA);
+        }else{
+            //权限已经开启，做相应事情
+            isPermissionGranted = true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == RC_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //权限被用户同意,做相应的事情
+                isPermissionGranted = true;
+            } else {
+                //权限被用户拒绝，做相应的事情
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //开始/停止推流
+            case R.id.publish:
+                if (mPublishBtn.getText().toString().contentEquals("开始")) {
+                    rtmpUrl = mRempUrlEt.getText().toString();
+                    if (TextUtils.isEmpty(rtmpUrl)) {
+                        Toast.makeText(getApplicationContext(), "地址不能为空！", Toast.LENGTH_SHORT).show();
+                    }
+                    mPublisher.startPublish(rtmpUrl);
+                    //mPublisher.startCamera();
+                    mPublisher.startRecord(path);
+
+                    if (mEncoderBtn.getText().toString().contentEquals("软编码")) {
+                        Toast.makeText(getApplicationContext(), "当前使用硬编码", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "当前使用软编码", Toast.LENGTH_SHORT).show();
+                    }
+                    mPublishBtn.setText("停止");
+                    mEncoderBtn.setEnabled(false);
+                } else if (mPublishBtn.getText().toString().contentEquals("停止")) {
+                    mPublisher.stopPublish();
+                    mPublisher.stopRecord();
+                    mPublishBtn.setText("开始");
+                    mEncoderBtn.setEnabled(true);
+                }
+                break;
+            //切换摄像头
+            case R.id.swCam:
+                mPublisher.switchCameraFace((mPublisher.getCameraId() + 1) % Camera.getNumberOfCameras());
+                break;
+            //切换编码方式
+            case R.id.swEnc:
+                if (mEncoderBtn.getText().toString().contentEquals("软编码")) {
+                    mPublisher.switchToSoftEncoder();
+                    mEncoderBtn.setText("硬编码");
+                } else if (mEncoderBtn.getText().toString().contentEquals("硬编码")) {
+                    mPublisher.switchToHardEncoder();
+                    mEncoderBtn.setText("软编码");
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        mPublisher.resumeRecord();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+//        mPublisher.pauseRecord();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        mPublisher.stopPublish();
+//        mPublisher.stopRecord();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+//        mPublisher.stopEncode();
+//        mPublisher.stopRecord();
+//        mPublisher.setScreenOrientation(newConfig.orientation);
+//        if (mPublishBtn.getText().toString().contentEquals("停止")) {
+//            mPublisher.startEncode();
+//        }
+//        mPublisher.startCamera();
+    }
+
+    @Override
+    public void onNetworkWeak() {
+        Toast.makeText(getApplicationContext(), "网络型号弱", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNetworkResume() {
+
+    }
+
+    @Override
+    public void onEncodeIllegalArgumentException(IllegalArgumentException e) {
+        handleException(e);
+    }
+
+    private void handleException(Exception e) {
+        try {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            mPublisher.stopPublish();
+            mPublisher.stopRecord();
+            mPublishBtn.setText("开始");
+        } catch (Exception e1) {
+            //
+        }
+    }
+
+    @Override
+    public void onRtmpConnecting(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRtmpConnected(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRtmpVideoStreaming() {
+
+    }
+
+    @Override
+    public void onRtmpAudioStreaming() {
+
+    }
+
+    @Override
+    public void onRtmpStopped() {
+        Toast.makeText(getApplicationContext(), "已停止", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRtmpDisconnected() {
+        Toast.makeText(getApplicationContext(), "未连接服务器", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRtmpVideoFpsChanged(double fps) {
+
+    }
+
+    @Override
+    public void onRtmpVideoBitrateChanged(double bitrate) {
+
+    }
+
+    @Override
+    public void onRtmpAudioBitrateChanged(double bitrate) {
+
+    }
+
+    @Override
+    public void onRtmpSocketException(SocketException e) {
+        handleException(e);
+    }
+
+    @Override
+    public void onRtmpIOException(IOException e) {
+        handleException(e);
+    }
+
+    @Override
+    public void onRtmpIllegalArgumentException(IllegalArgumentException e) {
+        handleException(e);
+    }
+
+    @Override
+    public void onRtmpIllegalStateException(IllegalStateException e) {
+        handleException(e);
+    }
+
+    @Override
+    public void onRecordPause() {
+        Toast.makeText(getApplicationContext(), "Record paused", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRecordResume() {
+        Toast.makeText(getApplicationContext(), "Record resumed", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRecordStarted(String msg) {
+        Toast.makeText(getApplicationContext(), "Recording file: " + msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRecordFinished(String msg) {
+        Toast.makeText(getApplicationContext(), "MP4 file saved: " + msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRecordIOException(IOException e) {
+        handleException(e);
+    }
+
+    @Override
+    public void onRecordIllegalArgumentException(IllegalArgumentException e) {
+        handleException(e);
+    }
+
+}
---
+++ vp/app/src/main/obj/local/armeabi/objs/sffstreamer/simplest_ffmpeg_streamer.o.d
@@ -0,0 +1,225 @@
+obj/local/armeabi/objs/sffstreamer/simplest_ffmpeg_streamer.o: \
+ jni/simplest_ffmpeg_streamer.c \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/include/libavcodec/avcodec.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ jni/include/libavutil/samplefmt.h jni/include/libavutil/avutil.h \
+ jni/include/libavutil/common.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h \
+ jni/include/libavutil/attributes.h jni/include/libavutil/version.h \
+ jni/include/libavutil/macros.h jni/include/libavutil/avconfig.h \
+ jni/include/libavutil/mem.h jni/include/libavutil/error.h \
+ jni/include/libavutil/rational.h jni/include/libavutil/mathematics.h \
+ jni/include/libavutil/intfloat.h jni/include/libavutil/log.h \
+ jni/include/libavutil/pixfmt.h jni/include/libavutil/old_pix_fmts.h \
+ jni/include/libavutil/attributes.h jni/include/libavutil/avutil.h \
+ jni/include/libavutil/buffer.h jni/include/libavutil/cpu.h \
+ jni/include/libavutil/channel_layout.h jni/include/libavutil/dict.h \
+ jni/include/libavutil/frame.h jni/include/libavutil/buffer.h \
+ jni/include/libavutil/dict.h jni/include/libavutil/samplefmt.h \
+ jni/include/libavutil/log.h jni/include/libavutil/pixfmt.h \
+ jni/include/libavutil/rational.h jni/include/libavcodec/version.h \
+ jni/include/libavutil/version.h jni/include/libavcodec/old_codec_ids.h \
+ jni/include/libavformat/avformat.h jni/include/libavformat/avio.h \
+ jni/include/libavutil/common.h jni/include/libavformat/version.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h \
+ D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdio.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/time.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/include/libavcodec/avcodec.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/errno.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+jni/include/libavutil/samplefmt.h:
+
+jni/include/libavutil/avutil.h:
+
+jni/include/libavutil/common.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/inttypes.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/limits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/math.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/stdlib.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/string.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/alloca.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/strings.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/memory.h:
+
+jni/include/libavutil/attributes.h:
+
+jni/include/libavutil/version.h:
+
+jni/include/libavutil/macros.h:
+
+jni/include/libavutil/avconfig.h:
+
+jni/include/libavutil/mem.h:
+
+jni/include/libavutil/error.h:
+
+jni/include/libavutil/rational.h:
+
+jni/include/libavutil/mathematics.h:
+
+jni/include/libavutil/intfloat.h:
+
+jni/include/libavutil/log.h:
+
+jni/include/libavutil/pixfmt.h:
+
+jni/include/libavutil/old_pix_fmts.h:
+
+jni/include/libavutil/attributes.h:
+
+jni/include/libavutil/avutil.h:
+
+jni/include/libavutil/buffer.h:
+
+jni/include/libavutil/cpu.h:
+
+jni/include/libavutil/channel_layout.h:
+
+jni/include/libavutil/dict.h:
+
+jni/include/libavutil/frame.h:
+
+jni/include/libavutil/buffer.h:
+
+jni/include/libavutil/dict.h:
+
+jni/include/libavutil/samplefmt.h:
+
+jni/include/libavutil/log.h:
+
+jni/include/libavutil/pixfmt.h:
+
+jni/include/libavutil/rational.h:
+
+jni/include/libavcodec/version.h:
+
+jni/include/libavutil/version.h:
+
+jni/include/libavcodec/old_codec_ids.h:
+
+jni/include/libavformat/avformat.h:
+
+jni/include/libavformat/avio.h:
+
+jni/include/libavutil/common.h:
+
+jni/include/libavformat/version.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/jni.h:
+
+D:/ndk/android-ndk-r10e/platforms/android-9/arch-arm/usr/include/android/log.h:
